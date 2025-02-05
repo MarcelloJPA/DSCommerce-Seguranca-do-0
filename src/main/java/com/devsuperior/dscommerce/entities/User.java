5778a1701a6aae32e6b34e6db6c4af7558dccf30
Marcello <cellolim4jpa@gmail.com>
2025-02-05 14:44:54 -0300

    Domain model User with Role, seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
@@ -24,7 +26,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
-
+    
+    @ManyToMany
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name= "role_id"))
+    private Set<Role> roles = new HashSet<>();
+    
     public User() {
     }
 
@@ -88,6 +95,25 @@ public class User {
     public List<Order> getOrders() {
         return orders;
     }
+    
+    public Set<Role> getRoles() {
+		return roles;
+	}
+    // Insert a role for a user
+
+    public void addRole(Role role) {
+    	roles.add(role);
+    }
+    
+    //Check if user has a role
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
     @Override
     public boolean equals(Object o) {
@@ -103,4 +129,7 @@ public class User {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+
+	
+        
 }
