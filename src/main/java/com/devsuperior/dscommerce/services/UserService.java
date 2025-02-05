d1ae0a196b59b53baabe5980812e6bc1373d58b9
Marcello <cellolim4jpa@gmail.com>
2025-02-05 19:39:47 -0300

    Add UserDetailsService

---
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+
+import projections.UserDetailsProjection;
+
+public class UserService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+		
+		if(result.size() == 0) {
+			throw new UsernameNotFoundException("Email not Found");
+		}
+		
+		User user = new User();
+		user.setEmail(result.get(0).getUsername());
+		user.setPassword(result.get(0).getPassword());
+		for(UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
+		return user;
+	}
+
+}
---
+++ src/main/java/projections/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package projections;
+
+public interface UserDetailsProjection {
+	
+	    String getUsername();
+	    String getPassword();
+	    Long getRoleId();
+	    String getAuthority();
+
+}
